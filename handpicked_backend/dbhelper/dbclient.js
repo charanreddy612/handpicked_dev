21aa7bac66c12a0ca8516973ed2cb1da2e6fae48
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2025-08-11 21:24:22 +0530

    enable supabse client debugging

--- handpicked_backend/dbhelper/dbclient.js
+++ handpicked_backend/dbhelper/dbclient.js
@@ -2,6 +2,8 @@ import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY,{
+    debug: true,
+});
 
 export { supabase }; 
