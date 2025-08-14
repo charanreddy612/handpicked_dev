b94d8ccd3f92d7f6cb8eb2a62332b5e69d67b224
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2025-08-14 22:13:28 +0530

    Blogs Upload issue

--- handpicked_backend/routes/blogRoutes.js
+++ handpicked_backend/routes/blogRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import * as blogController from "../controllers/blogController.js";
+import { uploadMemory } from "../middleware/uploadMemory.js";
 
 const router = Router();
 
@@ -19,13 +20,27 @@ router.get("/:id", blogController.getBlog);
  * POST /api/blogs
  * Create blog - handles image uploads to Supabase Storage
  */
-router.post("/", blogController.createBlog);
+router.post(
+  "/",
+  uploadMemory.fields([
+    { name: "featured_thumb", maxCount: 1 },
+    { name: "featured_image", maxCount: 1 }
+  ]),
+  blogController.createBlog
+);
 
 /**
  * PUT /api/blogs/:id
  * Update blog - optionally updates images
  */
-router.put("/:id", blogController.updateBlog);
+router.put(
+  "/:id",
+  uploadMemory.fields([
+    { name: "featured_thumb", maxCount: 1 },
+    { name: "featured_image", maxCount: 1 }
+  ]),
+  blogController.updateBlog
+);
 
 /**
  * PATCH /api/blogs/:id/status
