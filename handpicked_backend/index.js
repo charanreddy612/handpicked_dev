326b3ecaf81631472fa06f7a67f6fa5613ebc022
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2025-08-24 19:15:33 +0530

    Handpicked Client Backend Code commit

--- handpicked_backend/index.js
+++ handpicked_backend/index.js
@@ -110,6 +110,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
