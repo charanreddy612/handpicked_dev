3277c9798bfabdfc261b66b9e7d85c6e88ea41fa
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2025-09-24 17:54:09 +0530

    quill editor uphgrade

--- handpicked_startup/src/components/common/SafeQuill.jsx
+++ handpicked_startup/src/components/common/SafeQuill.jsx
@@ -10,14 +10,14 @@ const SafeQuill = forwardRef((props, ref) => {
     (async () => {
       try {
         // load Quill core + table plugin + styles (client only)
-        const QuillModule = await import("quill");
+        const QuillModule = await import(/* @vite-ignore */ "quill");
         const Quill = QuillModule.default || QuillModule;
 
         // register quill-better-table (if installed)
         try {
-          const QBetter = await import("quill-better-table");
+          const QBetter = await import(/* @vite-ignore */ "quill-better-table");
           const QBetterDefault = QBetter.default || QBetter;
-          await import("quill-better-table/dist/quill-better-table.css");
+          await import(/* @vite-ignore */ "quill-better-table/dist/quill-better-table.css");
           // register under modules/better-table
           Quill.register({ "modules/better-table": QBetterDefault }, true);
         } catch (tblErr) {
@@ -26,8 +26,8 @@ const SafeQuill = forwardRef((props, ref) => {
         }
 
         // now load the react-quill wrapper and theme css
-        const mod = await import("react-quill-new");
-        await import("react-quill-new/dist/quill.snow.css");
+        const mod = await import(/* @vite-ignore */ "react-quill-new");
+        await import(/* @vite-ignore */ "react-quill-new/dist/quill.snow.css");
 
         if (mounted) {
           setEditor(() => mod.default);
