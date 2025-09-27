1ef7ae276deb71017f4d3aa3f81e3df7b9d2172c
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2025-09-27 22:39:18 +0530

    testing for endpoint upgrade changes

--- handpicked_startup/src/config/api.js
+++ handpicked_startup/src/config/api.js
@@ -1,8 +1,8 @@
 // Production
-// export const API_BASE_URL = 'https://admin-api.savingharbor.com';
+export const API_BASE_URL = 'https://admin-api.savingharbor.com';
 
 // Development
-export const API_BASE_URL = 'https://savingharbor-admin-dev.onrender.com';
+// export const API_BASE_URL = 'https://savingharbor-admin-dev.onrender.com';
 
 // Local
 // export const API_BASE_URL = 'http://localhost:5000';
